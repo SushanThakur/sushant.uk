96be522ec247249d800f6bea9a0cd3a2d9ebfce9
Sushant Thakur <mail@sushant.name.np>
2024-09-08 19:28:48 +0545

    add mobile nav unoptimal

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,33 @@
 import { Route } from "react-router-dom";
+import menu from '../assets/menu.png';
+import close from '../assets/close.png';
 
 export default function NavBar() {
   return(
     <div className="nav-container">
       <div className="nav-name"><a href="#">Sushant Thakur</a></div>
-      <ul>
+      <ul className="desktop-view-nav">
         <a href="#"><li>Home</li></a>
         <a href="#projects"><li>Projects</li></a>
         <a href="#skills"><li>Skills</li></a>
         <a href="#contact"><li>Contact</li></a>
       </ul>
+      <div className="mobile-view-nav">
+      <div className="mobile-view-options" id="menu">
+          <img className="mobile-nav-options" src={menu} alt="menu"/>
+          <img className="mobile-nav-options" id="close" src={close} alt="close"/>
+        </div>
+        <ul>
+          <a href="#"><li>Home</li></a>
+          <a href="#projects"><li>Projects</li></a>
+          <a href="#skills"><li>Skills</li></a>
+          <a href="#contact"><li>Contact</li></a>
+        </ul>
+      </div>
     </div>
   )
 }
 
+const mobileNavDiv = document.getElementsByClassName("mobile-view-nav");
+const menuOption = document.getElementById("menu");
+const closeOption = document.getElementById("close");
