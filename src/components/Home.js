8da7ba70f10b7a7331b20b55fbdd0d7854e9c299
Sushant Thakur <mail@sushant.name.np>
2024-09-11 19:42:04 +0545

    responsiveness failed

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ export default function Home() {
       <section id="home">
         <div className="home-wrapper">
           <span>
-            <h1>Hi, I'm Sushant Thakur.</h1>
+            <h1>Hi, I'm Sushant.</h1>
             <h3>A hobbyist programmer and 3D Modeler.</h3>
           </span>
           <img src="https://ia601703.us.archive.org/6/items/twitter-default-pfp/e.png" 
